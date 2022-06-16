b1f59a2bd95ddbee21633707ca3abd37bd378229
Temu4 <artem.vorobjov@gmail.com>
2022-06-16 14:10:18 +0200

    provide PORT

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 // local modules
 const router = require('./src/router');
 
+const PORT = process.env.PORT || 80;
+
 const server = http.createServer(router);
 
-server.listen(80);
+server.listen(PORT);
